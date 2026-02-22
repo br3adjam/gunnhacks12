06f374ae589715b71eb1425290fafd4c256a716b
temujin-b <temujinbt@gmail.com>
2026-02-22 07:26:32 +0000

    create a gui

--- keyboard/src/main/java/gui.java
+++ keyboard/src/main/java/gui.java
@@ -1,2 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+
 public class gui {
+
+    public gui(){
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel(); 
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("OUR GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new gui();
+    }
 }
